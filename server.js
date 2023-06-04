36ebfe8a31af3c40c7352c1f41160ba0c1ea68aa
Illia Milintieiev <ilyamilint@gmail.com>
2023-06-04 14:54:17 +0300

    comit#2

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Ilia:NEWlife@cluster0.uvs9aws.mongodb.net/delivery";
+const { DB_HOST, PORT = 3000 } = process.env.DB_HOST;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
-    console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT);
+    console.log("Server running");
     console.log("Database connect success");
   })
   .catch((error) => console.log(error.message));
